7d2288e609cbe1d126762f287f4b6c8f324a8308
josemicarrion99 <jmcp25@alu.ua.es>
2021-03-11 00:48:46 +0100

    Creado el seeder de obras del museo

---
+++ database/seeds/ArtworksTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Artwork;
+
+class ArtworksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $aux = new Artwork();
+        $aux->id = 1;
+        $aux->title = 'La noche estrellada';
+        $aux->author = 'Vincent Van Gogh';
+        $aux->year = '1889';
+        $aux->save();
+
+        $aux = new Artwork();
+        $aux->id = 1;
+        $aux->title = 'El beso';
+        $aux->author = 'Gustav Klimt';
+        $aux->year = '1908';
+        $aux->save();
+
+        $aux = new Artwork();
+        $aux->id = 1;
+        $aux->title = 'Guernica';
+        $aux->author = 'Pablo Picasso';
+        $aux->year = '1937';
+        $aux->save();
+
+        $aux = new Artwork();
+        $aux->id = 1;
+        $aux->title = 'Las meninas';
+        $aux->author = 'Diego Velazquez';
+        $aux->year = '1656';
+        $aux->save();
+
+        $aux = new Artwork();
+        $aux->id = 1;
+        $aux->title = 'La joven de la perla';
+        $aux->author = 'Johannes Vermeer';
+        $aux->year = '1665';
+        $aux->save();
+
+    }
+}
