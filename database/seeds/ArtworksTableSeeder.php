ebfe8779d493e53be982254cb303784a5c43d185
josemicarrion99 <jmcp25@alu.ua.es>
2021-03-11 09:43:11 +0100

    Varios cambios

--- database/seeds/ArtworksTableSeeder.php
+++ database/seeds/ArtworksTableSeeder.php
@@ -20,28 +20,28 @@ class ArtworksTableSeeder extends Seeder
         $aux->save();
 
         $aux = new Artwork();
-        $aux->id = 1;
+        $aux->id = 2;
         $aux->title = 'El beso';
         $aux->author = 'Gustav Klimt';
         $aux->year = '1908';
         $aux->save();
 
         $aux = new Artwork();
-        $aux->id = 1;
+        $aux->id = 3;
         $aux->title = 'Guernica';
         $aux->author = 'Pablo Picasso';
         $aux->year = '1937';
         $aux->save();
 
         $aux = new Artwork();
-        $aux->id = 1;
+        $aux->id = 4;
         $aux->title = 'Las meninas';
         $aux->author = 'Diego Velazquez';
         $aux->year = '1656';
         $aux->save();
 
         $aux = new Artwork();
-        $aux->id = 1;
+        $aux->id = 5;
         $aux->title = 'La joven de la perla';
         $aux->author = 'Johannes Vermeer';
         $aux->year = '1665';
