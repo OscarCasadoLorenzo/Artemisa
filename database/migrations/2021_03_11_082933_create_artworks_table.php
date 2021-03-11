e410e6d46c01285c0416a5a0579493a370e5178e
josemicarrion99 <jmcp25@alu.ua.es>
2021-03-11 10:51:45 +0100

    Corregido el poner artwork en plural y year pasa de ser string a ser int

--- database/migrations/2021_03_11_082933_create_artworks_table.php
+++ database/migrations/2021_03_11_082933_create_artworks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArtworkTable extends Migration
+class CreateArtworksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateArtworkTable extends Migration
      */
     public function up()
     {
-        Schema::create('artwork', function (Blueprint $table) {
+        Schema::create('artworks', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
             $table->string('author');
-            $table->string('year');
+            $table->int('year');
         });
     }
 
@@ -28,6 +28,6 @@ class CreateArtworkTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('artwork');
+        Schema::dropIfExists('artworks');
     }
 }
