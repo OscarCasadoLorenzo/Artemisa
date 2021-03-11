2cc6666c0b3456e1034be05c2181beadcd709e55
ocl5 <ocl5@alu.ua.es>
2021-03-11 15:47:12 +0100

    relacion exponer

--- app/Artwork.php
+++ app/Artwork.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artwork extends Model
 {
-    //
+    public function museum(){
+        return $this->belongsTo('App\Museum');
+    }
 }
--- app/Museum.php
+++ app/Museum.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Museum extends Model
 {
-    //
+    public function artwork(){
+        return $this->hasMany('App\Artwork');
+    }
 }
