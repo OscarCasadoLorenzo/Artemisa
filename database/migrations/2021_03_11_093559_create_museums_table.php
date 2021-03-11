e8f71c4a20ee862fe485db7b72f12fcd5cd16d81
ocl5 <ocl5@alu.ua.es>
2021-03-11 10:38:35 +0100

    Rectificada migracion museums y modelo adjunto, dependencia doctrine/dbal

--- database/migrations/2021_03_11_093559_create_museums_table.php
+++ database/migrations/2021_03_11_093559_create_museums_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMuseumTable extends Migration
+class CreateMuseumsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateMuseumTable extends Migration
      */
     public function up()
     {
-        Schema::create('museum', function (Blueprint $table) {
+        Schema::create('museums', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->integer('capacity');
@@ -30,6 +30,6 @@ class CreateMuseumTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('museum');
+        Schema::dropIfExists('museums');
     }
 }
