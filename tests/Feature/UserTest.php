c517b699b39716815833ff1953a7324b72dec4ab
Girona98 <lgp90@alu.ua.es>
2021-03-11 15:44:35 +0100

    arreglo errores

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -3,24 +3,24 @@ namespace Tests\Units;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\User;
 
 class UserTest extends TestCase
 {
-    public function UserNameTest(){
+    public function testExample(){
         $usu = new User();
         $usu->nombre = 'Test';
-        $usu->apellido1 = '';
-        $usu->apellido2 = '';
+        $usu->apellido1 = 'test';
+        $usu->apellido2 = 'test';
         $usu->fecha_nac = '2021-01-01';
         $usu->dni = '12345678A';
         $usu->email = 'test@prueba.com';
         $usu->password = "#12341234_asdf";
-        $usu->localidad = 'prueba';
-        $usu->save();
+        $usu->localidad = 'prueba'; 
 
-        $this->assertEquals($user->nombre, 'Test');
-        $this->assertEquals($user->email, 'test@prueba.com');
-        $this->assertEquals($user->fecha_nac, '2021-01-01');
+        $this->assertEquals($usu->nombre, 'Test');
+        $this->assertEquals($usu->email, 'test@prueba.com');
+        $this->assertEquals($usu->fecha_nac, '2021-01-01');
     }
 }
 
