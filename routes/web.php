230d44495deb3f7c452509a1716258f37cda6833
josemicarrion99 <jmcp25@alu.ua.es>
2021-04-13 19:11:20 +0200

    Añadidos los where a las rutas

--- routes/web.php
+++ routes/web.php
@@ -16,29 +16,29 @@ Route::get('/', 'PagesController@show');
 
 //Rutas de extracción de listados
 Route::get('/users/', 'UserController@getUsers');
-Route::get('/museums/', 'MuseumController@getMuseums'); 
-Route::get('/authors/', 'AuthorController@getAuthors'); 
-Route::get('/artworks/', 'ArtworkController@getArtworks'); 
+Route::get('/museums/', 'MuseumController@getMuseums');
+Route::get('/authors/', 'AuthorController@getAuthors');
+Route::get('/artworks/', 'ArtworkController@getArtworks');
 
 //Inserción de elementos
-Route::post('/users', 'UserController@saveUser'); 
-Route::post('/museums', 'MuseumController@saveMuseum'); 
-Route::post('/authors', 'AuthorController@saveAuthors'); 
-Route::post('/collections', 'CollectionController@saveCollection'); 
-Route::post('/artworks', 'ArtworkController@saveArtworks'); 
+Route::post('/users', 'UserController@saveUser');
+Route::post('/museums', 'MuseumController@saveMuseum');
+Route::post('/authors', 'AuthorController@saveAuthors');
+Route::post('/collections', 'CollectionController@saveCollection');
+Route::post('/artworks', 'ArtworkController@saveArtworks');
 
 
 //Creación de elementos
 Route::get('/users/create', 'UserController@createUser');
-Route::get('/museums/create', 'MuseumController@createMuseum'); 
-Route::get('/collections/create', 'CollectionController@createCollection'); 
-Route::get('/artworks/create', 'ArtworkController@createArtwork'); 
-Route::get('/authors/create', 'AuthorController@createAuthor'); 
+Route::get('/museums/create', 'MuseumController@createMuseum');
+Route::get('/collections/create', 'CollectionController@createCollection');
+Route::get('/artworks/create', 'ArtworkController@createArtwork');
+Route::get('/authors/create', 'AuthorController@createAuthor');
 
 //RUTAS + COMPLEJAS
-Route::get('/museums/{id}', 'MuseumController@getMuseum'); 
-Route::get('/museums/{idM}/collections/{idC}', 'CollectionController@getCollection');
-Route::get('/museums/{idM}/collections/{idC}/artworks', 'ArtworkController@getArtworks');
+Route::get('/museums/{id}', 'MuseumController@getMuseum')->where('id', '\d+'); //solo permite que id sean numeros
+Route::get('/museums/{idM}/collections/{idC}', 'CollectionController@getCollection')->where('idM', '\d+');
+Route::get('/museums/{idM}/collections/{idC}/artworks', 'ArtworkController@getArtworks')->where('idM', '\d+');
 //Route::get('/museums/{idM}/collections/{idC}/artworks/{idA}', 'ArtworkController@getArtwork');
 
 
