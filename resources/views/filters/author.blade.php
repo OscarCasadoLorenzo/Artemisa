cd2eeb9481d3cd93c3e2cef224614a65d3ec633b
ocl5 <ocl5@alu.ua.es>
2021-05-14 15:16:25 +0200

    navbar authors css

--- resources/views/filters/author.blade.php
+++ resources/views/filters/author.blade.php
@@ -1,6 +1,19 @@
-<div class="col-sm-2">
+<style>
+    .textboxs{
+        display:flex;
+        margin-left:500px;
+    }
+
+    #boton{
+        height: 35px;
+        margin-top: 5px;
+        margin-right: 20px;
+    }
+</style>
+
+<div class="textboxs">
     <form action="/authors" method="get">
-        <h5> Nombre<input type="text" name="name"> </h5>
+        <input type="text" name="name"  placeholder="Nombre" id="boton">
         <button type="submit" class="btn btn-primary">Buscar</button>
     </form >
 </div>
