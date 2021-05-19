9c737ee4708282919c2ee893e15547545841c425
ocl5 <ocl5@alu.ua.es>
2021-05-19 14:51:58 +0200

    arreglando cosas

--- resources/views/listObjects/museum.blade.php
+++ resources/views/listObjects/museum.blade.php
@@ -7,6 +7,15 @@
         width:1000px !important;
         object-fit:fill;
     }
+
+    .grid-container{
+        display:grid;
+        grid-template-columns: 50% 50%; 
+    }
+
+    .grid-item{
+        display:flex;
+    }
 </style>
 
 @section('filters')
@@ -15,25 +24,29 @@
 
 @section('information')
 
-    @foreach($museums as $museum)
-        <div class="card" style="margin:10px;">
-            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light" style="margin:15px">
-                <a href="/museums/{{$museum->id}}">
-                    <img
-                        src= {{asset($museum->imgRoute)}}
-                        class="img-fluid"
-                        style="width:auto;"
-                    />
-                </a>
+    <div class="grid-container">
+        @foreach($museums as $museum)
+            <div class="grid-item">
+                <div class="card" style="margin:10px;">
+                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light" style="margin:15px">
+                        <a href="/museums/{{$museum->id}}">
+                            <img
+                                src= {{asset($museum->imgRoute)}}
+                                class="img-fluid"
+                                style="width:auto;"
+                            />
+                        </a>
 
-                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-            </div>
+                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                    </div>
 
-            <div class="card-body" style = "text-align:center;">
-                <h5 class="card-title">{{$museum->name}}</h5>
-                <p class="card-text">{{$museum->location}}</p>
-                <a href="/museums/{{$museum->id}}" class="btn btn-primary">Show collections</a>
+                    <div class="card-body" style = "text-align:center;">
+                        <h5 class="card-title">{{$museum->name}}</h5>
+                        <p class="card-text">{{$museum->location}}</p>
+                        <a href="/museums/{{$museum->id}}" class="btn btn-primary">Show collections</a>
+                    </div>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 @endsection
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -16,52 +16,158 @@
 
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"> -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
+        <script src="https://kit.fontawesome.com/177edab996.js" crossorigin="anonymous"></script>
 
         <title>Artemisa | @yield('title', 'Default')</title>
     </head>
 
     <style>
+        *{
+            margin:0px;
+            box-sizing:border-box;
+        }
+
         body{
- /*            text-align:center;
-            margin-left: 100px;
- */        }
-
-        .pat{
-            display: block;
+            font-family:sans-serif;
+            padding: 90px 20px 0;
         }
 
-        .content{
+        .header{
+            background-color:#337ab7;
+            position:fixed;
             width:100%;
-            margin:auto;
-/*             margin-left: 20px;
- */        }
-
-        #information{
-            margin-top:20px;
-            display:flex;
-            justify-content:center;
+            top:0;
+            left:0;
+            height:80px;
         }
 
-        #filters{
+        .nav{
             display:flex;
+            justify-content:space-between;
+
+            max-width: 992px;
+            margin: 0 auto;
         }
 
-        .dropbtn {
-            background-color: #04AA6D;
+        .nav-menu-link{
             color: white;
-            padding: 16px;
-            font-size: 16px;
-            border: none;
+            border-radius:3px;
+            padding: 8px 12px;
         }
 
-        .administration{
-            margin-left: 18px;
-            margin-right: 18px;
+        .nav-menu-link:hover,
+        .nav-menu-link_active{
+            text-decoration:none;
+            color:white;
+            background-color: #034574;
+            transition:0.5s;
         }
+
+        .logo{
+            font-size: 30px;
+            font-weight:bold;
+            padding: 0 40px;
+            line-height:80px;
+            color:white;
+            
+        }
+
+        .logo:hover{
+            color:white;
+            text-decoration:none;
+        }
+
+        .nav-menu{
+            display:flex;
+            margin-right:40px;
+            list-style:none;
+        }
+
+        .nav-menu-item{
+            font-size: 18px;
+            margin: 0 10px;
+            line-height: 80px;
+            text-transform: uppercase;
+            width: max-content;
+        }
+
+        .nav-toggle{
+            background:none;
+            border:none;
+            color:white;
+            font-size:30px;
+            padding: 0 20px;
+            line-height: 60px;
+
+            display:none;
+        }
+
+        @media (max-width: 768px){
+            
+            body{
+                padding-top: 70px;
+            }
+
+            .header{
+                height:60px;
+            }
+
+            .logo{
+                font-size: 25px;
+                padding: 0 20px;
+                line-height:60px;                
+            }
+
+            .nav-menu{
+                flex-direction: column;
+                margin-right:20px;
+                background-color: #2c3e50;
+                position:fixed;
+                left:0;
+                top: 60px;
+                width:100%;
+                align-items:center;
+                padding:20px 0;
+
+                height: calc(100% - 60px);
+                overflow-y: auto;
+            }
+
+            .nav-menu-item{
+                line-height: 70px;
+            }
+
+            .nav-menu-link:hover,
+            .nav-menu-link_active{
+                color: #83c5f7;
+                background: none
+            }
+
+            .nav-toggle{
+                display:block;
+            }
+        }
+
+
     </style>
+
     <body>
-        <section class="content" id="header">
+        <section class="header">
+
+        <nav class = "nav">
+            <a href="#" class="logo">ARTEMISA</a>
+            <button class="nav-toggle">
+                <i class="fas fa-bars"></i>
+            </button>
+            <ul class="nav-menu">
+                <li class = "nav-menu-item"><a href="#" class="nav-menu-link">Blog</a>
+                <li class = "nav-menu-item"><a href="#" class="nav-menu-link">Blog</a>
+                <li class = "nav-menu-item"><a href="#" class="nav-menu-link">Blog</a>
+                <li class = "nav-menu-item"><a href="#" class="nav-menu-link">Blog</a>
+            </ul>
+        </nav> 
+
+        <!--
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container" style=" width:100%;">
 
@@ -70,10 +176,10 @@
                         <a href="#" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-dark text-decoration-none dropdown-toggle"
                         id="dropdownNavLink" data-bs-toggle="dropdown" style="width: 35px !important; height: 35px !important; padding-top:1px" aria-expanded="false">
                         </a>
-                        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownNavLink" style="">
-                        <li><a class="dropdown-item" href="/authors">Authors</a></li>
-                        <li><a class="dropdown-item" href="/museums">Museums</a></li>
-                        </ul>
+                            <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownNavLink" style="">
+                                <li><a class="dropdown-item" href="/authors">Authors</a></li>
+                                <li><a class="dropdown-item" href="/museums">Museums</a></li>
+                            </ul>
                     </div>
 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -87,7 +193,7 @@
                                     <select name="action">
                                         <option hidden default value="/museums">Admin</option>
                                         <optgroup label="Users">
-                                            <!-- <option value="/users/create">Create</option> -->
+
                                             <option value="/users/update">Update</option>
                                             <option value="/users/delete">Delete</option>
                                         </optgroup>
@@ -116,13 +222,13 @@
                                 </form>
                             </li>
                         @endif
-                        <!-- </ul> -->
+
 
                         <form class="d-flex"  >
                             <section class="container" id="filters" style="width:100%;">
                                 @yield("filters")
                                 @if (Auth::check())
-                                    <!-- Botón user logeado -->
+                                  
                                     <div id="profile" class="dropdown" style="margin-top: 22px; display: flex; ">
                                         <button class="btn btn-primary" type="button" data-toggle="dropdown" >{{Auth::user()->name}}
                                         <span class="caret"></span></button>
@@ -141,7 +247,6 @@
                                         </ul>
                                     </div>
                                 @else
-                                <!-- botón para logear o registrarse -->
                                 <div class="container-fluid" style="margin-top: 22px; display: flex; ">
                                     <li class="nav-item">
                                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -157,6 +262,7 @@
                     </div>
                 </div>
             </nav>
+            -->
 
         </section>
 
