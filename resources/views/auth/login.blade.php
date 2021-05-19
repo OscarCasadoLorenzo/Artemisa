0bd884d458d34f92ad6c7afc1de4991c1f69f309
Girona98 <lgp90@alu.ua.es>
2021-05-19 17:50:22 +0200

    una poca de padding

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,17 +54,17 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-0" >
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                            </div>
+                    
+                            <div style="display:flex; justify-content:right; padding-right: 15px">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
                             </div>
                         </div>
                     </form>
