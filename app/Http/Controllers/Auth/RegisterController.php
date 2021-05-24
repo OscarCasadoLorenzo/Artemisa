7bc0d0cd728e3e0b4cd62ecb0fe0f8e9b0944673
josemicarrion99 <jmcp25@alu.ua.es>
2021-05-24 17:46:31 +0200

    Retoque validación

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,9 +51,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'regex:/^[a-zA-Z]+$/u|min:3|max:50|required',
-            'surname1' => 'regex:/^[a-zA-Z]+$/u|min:3|max:50|required',
-            'surname2' => 'regex:/^[a-zA-Z]+$/u|min:3|max:50|required',
+            'name' => 'regex:/^[a-zA-Z]+$/u|min:2|max:50|required',
+            'surname1' => 'regex:/^[a-zA-Z]+$/u|min:2|max:50|required',
+            'surname2' => 'regex:/^[a-zA-Z]+$/u|min:2|max:50|required',
             'email' => 'required|unique:users',
             'birth_date' => 'date',
             'location' => 'regex:/^[a-zA-Z]+$/u|min:3|max:75|required',
